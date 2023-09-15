7f56b2e2017d1040ba5dd7a20a4d1ae13bed20ba
IlyaMelnichenko <124725419+IlyaMelnichenko@users.noreply.github.com>
2023-09-15 20:17:12 +0300

    a

--- src/components/Cast.js
+++ src/components/Cast.js
@@ -1,6 +1,24 @@
+import { getReviews } from "fetch";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+
 export const Cast=()=>{
-    
+  //   const [reviews, setReviews] = useState(null);
+  // const { id } = useParams();
+  // useEffect(() => {
+  //   async function createRewies() {
+  //     try {
+  //       const data = await getReviews(id);
+  //       if (data.results.length !== 0) {
+  //         setReviews(data.results);
+  //       }
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   }
+  //   createRewies();
+  // }, []);
     return(
-        <>zalupa</>
+        <>df</>
     )
 }
